f19983278ba176b477536ee071d7cfae44b93a01
Glory Sinkhonde <sinkhondeglory@gmail.com>
2022-11-01 15:36:18 +0200

    Customised the carousel more

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -28,13 +28,31 @@ function Popular() {
     //getting the Api data
 
     const getPopular = async () => {
-        const getApi = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
+
+        //below we are checking the local storage to see if there are any items and if there are, we are setting our setpopular/where we are on the page to it and if there are no files in the local storage, we are fetching the data and setting the output to the local storage
+
+        //because the local storage can only store strings, when fetching, we stringify the JSON before setting it in local storage and after it is stored, it needs to turn back into an array before being used and that's why we parsed it back into an array for use
+
+        //while working on this project, I encountered a bug where the popular was returning undefined and I figured out, it was because I reached the total number of requests from the API, proud of myself everytime I figure out a problem like this
+
+        const check = localStorage.getItem("popular")
+
+        if(check){
+            setPopular(JSON.parse(check))
+        }else{
+            const getApi = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
         
-        const data = await getApi.json();
+            const data = await getApi.json();
 
-        // never use popular = data... go through the function instead
+            localStorage.setItem("popular", JSON.stringify(data.recipes))
 
-        setPopular(data.recipes)
+             // never use popular = data... go through the function instead
+
+             setPopular(data.recipes)
+             console.log(data.recipes)
+        }
+
+
     }
 
   return (
@@ -49,14 +67,21 @@ function Popular() {
         <Wrapper>
             <h3>Popular Picks</h3>
  
-                <Splide>
+                <Splide options={{
+                    perPage: 3,
+                    arrows: false,
+                    pagination: false,
+                    drag: 'free',
+                    gap: '5rem',
+                }}>
                   {popular.map(recipe =>{
                      return(
  
-                         <SplideSlide>
+                         <SplideSlide key={recipe.id}>
                             <Card>
                               <h4>{recipe.title}</h4>
                               <img src={recipe.image} alt={recipe.title} />
+                              <Gradient/>
                             </Card>
                          </SplideSlide>
                         
@@ -78,10 +103,43 @@ const Card = styled.div`
     min-height: 25rem;
     border-radius: 2rem;
     overflow: hidden;
+    position: relative;
 
     img{
         border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
     }
+
+    h4{
+        position: absolute;
+        z-index: 10;
+        left: 30%;
+        bottom: 0%;
+        transform: translate(-50, 0);
+        color: white;
+        width: 50%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center; 
+    };
+
+`
+
+const Gradient = styled.div`
+
+   z-index: 3;
+   position: absolute;
+   width: 100%;
+   height: 100%;
+   background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.6))  
 
 `
 
